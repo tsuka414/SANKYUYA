be20269f09e45354bf26b78b6e9c3a9772223d47
Keita <keita04141537@icloud.com>
2020-04-28 15:00:20 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
-     redirect_to @user
+      redirect_to @user
     else
       render :new
     end
