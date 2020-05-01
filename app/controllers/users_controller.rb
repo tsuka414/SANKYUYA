5b888829ac5814a684c004b6271eb224e2581892
Keita <keita04141537@icloud.com>
2020-05-01 10:55:49 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
@@ -25,29 +27,28 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "ユーザーを更新しました。"
+      flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
-  
+
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
-  
+
   def edit_basic_info
   end
-  
+
   def update_basic_info
     if @user.update_attributes(basic_info_params)
       flash[:success] = "#{@user.name}の基本情報を更新しました。"
@@ -62,15 +63,19 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-    
+
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
-    
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
-    
+
+    # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
         store_location
@@ -78,12 +83,14 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-    
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+
+    # システム管理権限所有かどうか判定します。
     def admin_user
-      redirect_to root url unless current_user.admin?
+      redirect_to root_url unless current_user.admin?
     end
 end
