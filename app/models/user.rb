6e0ec0ce69872787cdeb8a102e95fc84d633966b
Keita <keita04141537@icloud.com>
2020-05-06 14:59:15 +0000

    80%

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,12 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  def self.search(search) #ここでのself.はMicropost.を意味する
+    if search
+      where([ 'name LIKE ?', "%#{search}%"]) #検索とcontentの部分一致を表示。Micropost.は省略。
+    else
+      all #全て表示。Micropost.は省略。
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20200430052650) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-04-30 23:00:00"
-    t.datetime "work_time", default: "2020-04-30 22:30:00"
+    t.datetime "basic_time", default: "2020-05-02 23:00:00"
+    t.datetime "work_time", default: "2020-05-02 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
