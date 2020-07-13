4a49e26bcc2f7bd68279fbf6daddabb79f419e7b
EC2 Default User <ec2-user@ip-172-31-15-160.ap-northeast-1.compute.internal>
2020-07-13 17:04:56 +0000

    start

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 require 'rails/all'
+require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +11,7 @@ module AttendanceApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
